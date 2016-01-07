fc5c979b980f06877ac441686ad997dbf058402c
David Singer <david@dsinger.com>
2016-01-06 21:58:00 -0800

    Log stderr, too.

--- mpmonitord.py
+++ mpmonitord.py
@@ -5,9 +5,10 @@ import sys
 from mpmonitor import do_main_program
 
 logf = open('/home/david/src/radio/log.txt', 'a')
+errlogf = open('/home/david/src/radio/errlog.txt', 'a')
 
 
-with daemon.DaemonContext(stdout=logf,working_directory="/home/david/src/radio",initgroups=False):
+with daemon.DaemonContext(stdout=logf,stderr=errlogf,working_directory="/home/david/src/radio",initgroups=False):
     do_main_program()
 
 print "done"
