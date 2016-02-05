f482c61dc7e08195d263352ebf278d98069eb2e0
David Singer <david@dsinger.com>
2016-02-05 14:14:59 -0800

    Make the X10 daemon work better as a real daemon

--- x10d.py
+++ x10d.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python
 """ Daemonize the X10 Listener """
 import daemon
+import daemon.pidfile
 import sys
+import pwd
+import os
 from x10listener import do_main_program
 
-logf = open('/home/david/src/radio/dlog.txt', 'a')
 
+user = pwd.getpwnam('david')   # This is kinda ugly...
+workdir = os.path.join(user.pw_dir,'src/radio') 
+pidfile = os.path.join(workdir,'x10d.pid')
+uid = user.pw_uid
+gid = user.pw_gid
+logf = open(os.path.join(workdir, 'dlog.txt'),'a')
 
-with daemon.DaemonContext(stdout=logf,working_directory="/home/david/src/radio/"):
+with daemon.DaemonContext(stdout=logf,working_directory=workdir,pidfile=daemon.pidfile.PIDLockFile(pidfile),uid=uid,gid=gid):
     do_main_program()
 
 print "done"
