c769bdfa117f1c6917b71c161125d059a4ba4f05
David Singer <david@dsinger.com>
2016-03-16 18:37:20 -0700

    Patch in WeMo support

--- x10listener.py
+++ x10listener.py
@@ -3,6 +3,8 @@
 import sys
 from mysocket import *
 import socket
+import subprocess
+from ouimeaux.environment import Environment
 
 def sendcmd(cmd):
     # We don't care about replies, so just blast it and close
@@ -13,7 +15,7 @@ def sendcmd(cmd):
     sock.close()
 
 
-def handlex10line(l,s):
+def handlex10line(l,s, switch):
     """ X10 lines have this form:
     01/08 15:47:00 Rx RF HouseUnit: D5 Func: Off
 
@@ -40,6 +42,12 @@ def handlex10line(l,s):
                     sendcmd('next')
             elif hu == 'D5':
                 s.send('RF D5 ' + func + '\n')
+            elif hu in ('D1', 'D8', 'D16'):
+                # Need to bridge to the WeMo switch
+                if (func == 'Off'):
+                    switch.off()
+                else:
+                    switch.on()
         except:
             pass   # We don't care!
 
@@ -48,12 +56,16 @@ def handlex10line(l,s):
 def do_main_program():
     sys.stdout.close()
     sys.stdout = open('/home/david/src/radio/xlog.txt', 'a')
+    env = Environment(with_discovery=False,with_subscribers=False)
+    env.start()
+    env.discover(seconds=3)
+    switch = env.get_switch('streaming')
     s = mysocket()
     s.connect(('localhost', 1099))    # Connect to the X10 via mochad
     l = s.readline()
     while len(l) > 0:
         print l; sys.stdout.flush()
-        handlex10line(l,s)
+        handlex10line(l,s, switch)
         l = s.readline()
 
 if __name__ == "__main__":
